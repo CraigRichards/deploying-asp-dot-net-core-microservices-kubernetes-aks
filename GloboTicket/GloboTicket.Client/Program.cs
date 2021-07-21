cbfa167dfbecbed0b366837edd50792d31d44c35
Craig Richards <CraigAnthonyRichards@gmail.com>
2021-07-21 13:39:29 +1000

    Minor patches

--- GloboTicket/GloboTicket.Client/Program.cs
+++ GloboTicket/GloboTicket.Client/Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,6 +10,13 @@ namespace GloboTicket.Web
     {
         public static void Main(string[] args)
         {
+            ServicePointManager.ServerCertificateValidationCallback =
+                delegate (object sender, X509Certificate certificate, X509Chain
+                    chain, SslPolicyErrors sslPolicyErrors)
+                {
+                    return true;
+                };
+
             CreateHostBuilder(args).Build().Run();
         }
 
